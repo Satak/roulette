f22f069508895c26903a7cf8e2c194fd9de322c8
Sami Koskivaara <sami.koskivaara@gmail.com>
2018-01-07 18:58:35 +0200

    refactoring and odd/even implemented

---
+++ src/bet_types.py
@@ -0,0 +1,19 @@
+"""Bet types."""
+
+BET_TYPES = [
+    {
+        'id': 1,
+        'name': 'Straight-up',
+        'pay': 36
+    },
+    {
+        'id': 2,
+        'name': 'Red or Black',
+        'pay': 2
+    },
+    {
+        'id': 3,
+        'name': 'Odd or Even',
+        'pay': 2
+    }
+]
